130c6be7043b10d01121eb5e94f323cdbbed68aa
baghdat-bm <80079531+baghdat-bm@users.noreply.github.com>
2025-03-25 12:21:45 +0500

    Update copy-base.ts

--- lib/service/copy-base.ts
+++ lib/service/copy-base.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import https from 'https';
 
-export const API_BASE_URL = "http://localhost:8000/ru/school-api"; // url справочников
-export const API_DOCS_URL = "http://localhost:8000/ru/food-api"; // url документов
+export const API_BASE_URL = "https://37.151.91.88/ru/school-api"; // url справочников
+export const API_DOCS_URL = "https://37.151.91.88/ru/food-api"; // url документов
 
 
 export const api = axios.create({
@@ -10,4 +11,7 @@ export const api = axios.create({
         "Accept": "application/json",
         "Content-Type": "application/json;charset=UTF-8",
     },
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+    })
 });
